602e98515231642c45294c36f5408db19c62ee59
Tom Brückner <tom.brueckner@gmx.net>
2015-09-05 19:51:50 +0200

    Added JS implementation

---
+++ www/messages.js
@@ -0,0 +1,30 @@
+var exec = require("cordova/exec");
+
+/**
+ * Constructor.
+ *
+ * @returns {Messages}
+ */
+function Messages() {}
+
+/**
+ * Go to launch page page of native app
+ */
+Messages.prototype.gotoLaunchPage = function(successCallback, errorCallback) {
+
+  var options = [];
+
+  if (typeof errorCallback !== 'function') {
+    errorCallback = function() {};
+  }
+
+  if (typeof successCallback !== "function") {
+    successCallback = function() {};
+  }
+
+  exec(successCallback, errorCallback, 'ConcatsMessages', 'gotoLaunchPage',
+    options);
+};
+
+var messages = new Messages();
+module.exports = messages;
